76f438e048f3088f484e138101a444cd59b6ac79
skinnertx <65523964+skinnertx@users.noreply.github.com>
2024-01-04 02:13:57 -0600

    Basic Django tutorial

--- SMAAMsite/polls/urls.py
+++ SMAAMsite/polls/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
+    # ex: /polls/5/
+    # adding the 'name' argument to the path() allows me to access it using the {% url %} template tag in templates
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
